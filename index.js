c4ab0093b09549449c2c8c8eb5e82dd64ff198ce
Vallid9 <147008686+Vallid9@users.noreply.github.com>
2023-12-06 12:25:37 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
-const imageInput = document.getElementById("imageInput");
-const predictButton = document.getElementById("predictButton");
-const resultText = document.getElementById("resultText");
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  ...
+</head>
+<body>
+  ...
+  <script>
+    document.addEventListener("DOMContentLoaded", () => {
+      const imageInput = document.getElementById("imageInput");
+      const predictButton = document.getElementById("predictButton");
+      const resultText = document.getElementById("resultText");
 
-let model; // Variable to store loaded ONNX model
+      let model; // Variable to store loaded ONNX model
 
-async function loadModel() {
-  model = await onnx.load("Faceemotion_recognition_model.onnx");
-}
+      async function loadModel() {
+        model = await onnx.load("Faceemotion_recognition_model.onnx");
+      }
 
-predictButton.addEventListener("click", async () => {
-  await loadModel(); // Load model once
+      predictButton.addEventListener("click", async () => {
+        await loadModel(); // Load model once
 
-  const imageFile = imageInput.files[0];
-  const reader = new FileReader();
+        const imageFile = imageInput.files[0];
+        const reader = new FileReader();
 
-  reader.onload = async (event) => {
-    const imageData = event.target.result;
-    const prediction = await predictEmotion(imageData);
-    resultText.innerText = `Predicted emotion: ${prediction}`;
-  };
+        reader.onload = async (event) => {
+          const imageData = event.target.result;
+          const prediction = await predictEmotion(imageData);
+          resultText.innerText = `Predicted emotion: ${prediction}`;
+        };
 
-  reader.readAsDataURL(imageFile);
-});
+        reader.readAsDataURL(imageFile);
+      });
 
-async function predictEmotion(imageData) {
-  // Preprocess image according to your model requirements
-  const imageTensor = preprocessImage(imageData);
+      async function predictEmotion(imageData) {
+        // Preprocess image according to your model requirements
+        const imageTensor = preprocessImage(imageData);
 
-  // Run inference using the loaded model
-  const output = await model.run(null, { input: imageTensor });
+        // Run inference using the loaded model
+        const output = await model.run(null, { input: imageTensor });
 
-  // Get and process output
-  const predictions = output[0];
-  const predictedClass = getPredictedClass(predictions);
+        // Get and process output
+        const predictions = output[0];
+        const predictedClass = getPredictedClass(predictions);
 
-  // Return the corresponding emotion name
-  return getEmotionName(predictedClass);
-}
+        // Return the corresponding emotion name
+        return getEmotionName(predictedClass);
+      }
+    });
+  </script>
+</body>
+</html>
